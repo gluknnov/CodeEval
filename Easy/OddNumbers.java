72751951440afe63f7df1c351abed7f355ad4b69
gluknnov <gluknnov@gmail.com>
2016-01-07 15:42:35 +0300

    Create OddNumbers.java

---
+++ Easy/OddNumbers.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        for(Integer i = 1; i <= 99; i += 2) {
+            System.out.println(i.toString());
+        }
+    }
+}
