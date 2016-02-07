3dbf1839fde4fb9f091d3c7ad79df9839c4dab6c
gluknnov <gluknnov@gmail.com>
2016-02-07 19:55:13 +0300

    Create OddNumbers.cpp

---
+++ Easy/OddNumbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    for(int i = 1; i <= 99; i += 2) {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
